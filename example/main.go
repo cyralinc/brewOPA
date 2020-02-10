f8726320acd2bcca5c6cf317063918995e958da3
Timothy Nguyen <nguyen.d.timothy@gmail.com>
2020-02-09 21:20:02 -0800

    update example program

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 
@@ -9,11 +10,13 @@ import (
 )
 
 func main() {
+	// instantiate validator
 	validator, err := brewOPA.NewValidatorFromRego("../rego/brewOPA.rego")
 	if err != nil {
 		fmt.Print("failed to create validator: %v", err)
 	}
 
+	// read and add data access policy (multiple policies can be added to a validator)
 	data, err := ioutil.ReadFile("../rego/sample_data/policy.yaml")
 	if err != nil {
 		fmt.Printf("failed to decode yaml: %v", err)
@@ -32,19 +35,86 @@ func main() {
 		return
 	}
 
-	ctx := context.Background()
-
-	access := brewOPA.NewAccess("invoices", "bob", brewOPA.AccessTypeRead,
+	// this access reads from card_number, which is disallowed by the policy
+	badAccess := brewOPA.NewAccess("invoices", "bob", brewOPA.AccessTypeRead,
 		brewOPA.TablesReferenced([]string{"finance.cards"}),
 		brewOPA.ColumnsReferenced(map[string][]string{
 			"finance.cards": []string{"card_number", "credit_limit"},
 		}),
 	)
 
-	result, err := validator.Validate(ctx, access)
+	// validate access against stored policies
+	result, err := validator.Validate(context.Background(), badAccess)
 	if err != nil {
 		fmt.Printf("failed to validate access: %v", err)
 		return
 	}
-	fmt.Printf("result (%T): %+v", result, result)
+
+	b, err := json.Marshal(result)
+	if err != nil {
+		fmt.Printf("failed to encode result: %v", err)
+		return
+	}
+	fmt.Printf("result:\n%s\n\n", b)
+
+	// The access violates the policy!
+	// {
+	// 	"pass": false,
+	// 	"tables": {
+	// 		"finance.cards": {
+	// 			"policyDefined": true,
+	// 			"rulesApplied": {
+	// 				"reads": {
+	// 					"violated": true,
+	// 					"contextedRule": {
+	// 						"allow": true,
+	// 						"rows": 10,
+	// 						"attributes": ["credit_limit","card_family"]
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// Let's try again with a good access (no longer reading "card_number")
+	goodAccess := brewOPA.NewAccess("invoices", "bob", brewOPA.AccessTypeRead,
+		brewOPA.TablesReferenced([]string{"finance.cards"}),
+		brewOPA.ColumnsReferenced(map[string][]string{
+			"finance.cards": []string{"credit_limit"},
+		}),
+	)
+
+	result, err = validator.Validate(context.Background(), goodAccess)
+	if err != nil {
+		fmt.Printf("failed to validate access: %v", err)
+		return
+	}
+
+	b, err = json.Marshal(result)
+	if err != nil {
+		fmt.Printf("failed to encode result: %v", err)
+		return
+	}
+	fmt.Printf("result:\n%s\n\n", b)
+
+	// The access passes validation.
+	// {
+	// 	"pass": true,
+	// 	"tables": {
+	// 		"finance.cards": {
+	// 			"policyDefined": true,
+	// 			"rulesApplied": {
+	// 				"reads": {
+	// 					"violated": false,
+	// 					"contextedRule": {
+	// 						"allow": true,
+	// 						"rows": 10,
+	// 						"attributes": ["credit_limit", "card_family"]
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
